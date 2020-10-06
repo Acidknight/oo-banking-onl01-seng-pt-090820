d7dba56257742f08da43da9143ba2d7806803561
Anthony Leon <acidknights89@gmail.com>
2020-10-06 01:22:38 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
   end
   
   def execute_transaction
-    if @sender.balance < @amount
+    if @sender.status = false
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
 
