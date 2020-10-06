dd77eb52739a6cc4d6408da2b7df6bd4ffec6ce8
Anthony Leon <acidknights89@gmail.com>
2020-10-06 01:57:40 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,7 +22,7 @@ class Transfer
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-    elsif valid?.false
+    elsif valid? = false
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
